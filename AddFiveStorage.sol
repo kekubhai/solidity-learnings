ffa66a1740a813c3f59d1b446c42a389710906f7
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-09-29 21:51:53 +0530

    added store utility

--- AddFiveStorage.sol
+++ AddFiveStorage.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-import {SimpleStorage} from "./simplestorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
 contract AddFiveStorage is SimpleStorage {
 // +5
-
-    
+//overrides
+// virtual override
+  function store(uint256 _newNumber) public override {
+   favouriteNumber=_newNumber +5;
+  }
 }
